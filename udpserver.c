fffbd31fd0f4df249072c4cfb4be0687faf40c72
Johnny H. Nguyen <klanerz93@gmail.com>
2017-03-01 20:12:14 -0800

    Added number detection capability

--- udpserver.c
+++ udpserver.c
@@ -11,73 +11,63 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
-
-int main(int argc, char *argv[])
+#include <ctype.h>
+main(int argc, char *argv[]){
+  int sock;
+  int addr_len,bytes_read;
+  int serverPort;
+  char recv_data[1024];
+  struct sockaddr_in server_addr , client_addr;
+   if(argc != 2){
+       fprintf(stderr,"Invalid input\nUsage: ./a.out udpserver [Listen Port]");
+       exit(1);
+     }
+   serverPort = atoi(argv[1]);
+   if(serverPort > 65535 ) {
+       perror("Server port exceeds 16-bit range\n");
+       exit(1);
+     }
+   if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+       perror("Socket");
+       exit(1);
+     }
+server_addr.sin_family = AF_INET;
+server_addr.sin_port = htons(serverPort);
+server_addr.sin_addr.s_addr = INADDR_ANY;
+bzero(&(server_addr.sin_zero),8);
+   if (bind(sock,(struct sockaddr *)&server_addr,
+    sizeof(struct sockaddr)) == -1)        {
+       perror("Bind");
+       exit(1);
+     }
+addr_len = sizeof(struct sockaddr);
+printf("UDPServer Waiting for client on port %d\n", serverPort);
+fflush(stdout);
+while (1)
 {
-	int sock;
-        int addr_len, bytes_read;
-        int serverPort;
-        char recv_data[1024];
-        struct sockaddr_in server_addr , client_addr;
-        
-        if(argc != 2){
+  bytes_read = recvfrom(sock,recv_data,1024,0,
+(struct sockaddr *)&client_addr, &addr_len);
+  recv_data[bytes_read] = '\0';
 
-            fprintf(stderr,"Invalid input\nUsage: ./a.out udpserver [Listen Port]");
-            exit(1);
-        }
-        serverPort = atoi(argv[1]);
-        if(serverPort > 65535 )
-        {
-            perror("Server port exceeds 16-bit range\n");
-            exit(1);
-        }
-        if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-            perror("Socket");
-            exit(1);
-        }
+    if(isdigit(recv_data[0])) {
+      int number = atoi(recv_data);
+      printf("Number Received: %d\n", number);
 
-        server_addr.sin_family = AF_INET;
-        server_addr.sin_port = htons(serverPort);
-        server_addr.sin_addr.s_addr = INADDR_ANY;
-        bzero(&(server_addr.sin_zero),8);
-
-
-        if (bind(sock,(struct sockaddr *)&server_addr,
-            sizeof(struct sockaddr)) == -1)
-        {
-            perror("Bind");
-            exit(1);
-        }
-
-        addr_len = sizeof(struct sockaddr);
-		
-	printf("UDPServer Waiting for client on port %d\n",serverPort);
-        fflush(stdout);
-
-	while (1)
-	{
-
-          bytes_read = recvfrom(sock,recv_data,1024,0,
-	                    (struct sockaddr *)&client_addr, &addr_len);
-	  
-
-	  recv_data[bytes_read] = '\0';
-		
-          printf("(%s , %d) said : %s",inet_ntoa(client_addr.sin_addr),
-                                       ntohs(client_addr.sin_port), recv_data);
-          if( isdigit(recv_data))
-          {                             
-          printf("Number Received: %s", recv_data);
-          int y = atoi(recv_data*recv_data);
-          printf("The square of %s is %d",recv_data,y);
-          }
-          else if(recv_data == "EXITING ON CLIENT REQUEST")
-          {
-              printf("Exiting on client request");
-              return 0;
-          }
-	  fflush(stdout);
-
-        }
-        return 0;
+      int y = number*number;
+      printf("The square of %d is %d\n",number,y);
+    }
+   // else if(recv_data == "EXITING ON CLIENT REQUEST")
+   // {
+   //   printf("Exiting on client request");
+   //   return 0;
+   // }
+    else
+    {
+      printf("(%s , %d) said :",inet_ntoa(client_addr.sin_addr),
+      ntohs(client_addr.sin_port));
+      printf("%s",recv_data);
+    }
+    fflush(stdout);
+}
+ return 0;
 }
