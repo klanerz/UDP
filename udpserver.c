7dd263164f2fa5786aa45ea36c25544cfa503e89
Johnny H. Nguyen <klanerz93@gmail.com>
2017-03-01 16:59:24 -0800

    serverPort added with checks

--- udpserver.c
+++ udpserver.c
@@ -12,21 +12,32 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-        int sock;
+	int sock;
         int addr_len, bytes_read;
+        int serverPort;
         char recv_data[1024];
         struct sockaddr_in server_addr , client_addr;
+        
+        if(argc != 2){
 
-
+            fprintf(stderr,"Invalid input\nUsage: ./a.out udpserver [Listen Port]");
+            exit(1);
+        }
+        
+        if(serverPort > 65535 )
+        {
+            perror("Server port exceeds 16-bit range\n");
+            exit(1);
+        }
         if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
             perror("Socket");
             exit(1);
         }
 
         server_addr.sin_family = AF_INET;
-        server_addr.sin_port = htons(0);
+        server_addr.sin_port = htons(serverPort);
         server_addr.sin_addr.s_addr = INADDR_ANY;
         bzero(&(server_addr.sin_zero),8);
 
